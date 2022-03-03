700f781e6f1b30441356b93c98c81c7ec218804c
JailsonSantos <jailson.ads007@gmail.com>
2022-03-03 18:02:27 -0300

    Desafio finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,8 @@ import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  const cartSize = cart.length;
 
   return (
     <Container>
@@ -16,11 +16,12 @@ const Header = (): JSX.Element => {
         <img src={logo} alt="Rocketshoes" />
       </Link>
 
+
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
